e24e4c393846f23de35cd39dc220066adf14c123
brimandar <50734135+brimandar@users.noreply.github.com>
2019-09-07 14:27:31 +0200

    ajout nouveaute

---
+++ js/scrolling.js
@@ -0,0 +1,30 @@
+/**
+* Smooth scrolling to a specific element 
+**/
+function scrollTo( target ) {
+    let positionElt = target.offset().top
+    $("html, body").animate( { scrollTop: positionElt - 75+'px' }, 1500);
+};
+// Evenements sur clic des éléments nav pour un défilement en douceur vers l'ancre de la page 
+$("#linkPresentation").on("click", function() {
+    scrollTo( $("#presentation") ) 
+});
+
+$("#linkCarousel").on("click", function() {
+    scrollTo( $("#sectionCarousel") ) 
+});
+
+$("#linkCarte").on("click", function() {
+    scrollTo( $("#carteEtInformations") ) 
+});
+
+$("#linkContact").on("click", function() {
+    scrollTo( $("#contact") ) 
+});
+
+$("#linkLogo").on("click", function(){
+    /**
+    * Smooth scrolling to the top of page
+    **/
+   $("html, body").animate({scrollTop : 0}, 1500);
+})
